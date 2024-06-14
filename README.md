0635930e8333032ab196671b8358fd197501b48c
Thijs Leegwater <t.leegwater@picturae.com>
2024-06-14 10:28:56 +0200

    untested

---
+++ README.md
@@ -0,0 +1,2 @@
+# RotaryGSM
+## Converts a Ericsson type 1951 rotary phone to a GSM phone
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,95 @@
 #include <Arduino.h>
+#include "Sim800L.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#define RX  10
+#define TX  11
+
+Sim800L GSM(RX, TX);
+
+enum State {
+  STATE_IDLE,
+  STATE_RINGING,
+  STATE_CALLING
+};
+
+State currentState = STATE_IDLE;
+const int hookPin = 7;
+const int bellPin = 5;
+const int rotaryPin = 2;
+int rotaryPulseCount = 0;
+unsigned long lastRotaryPulse = 0;
+
+bool isHookOff() {
+    return digitalRead(hookPin);
+}
+
+void ringBell(boolean enable) {
+    digitalWrite(bellPin, enable);
+}
+
+void onRotaryPulse() {
+    if (lastRotaryPulse + 100 < millis()) {
+        rotaryPulseCount++;
+    } else {
+        Serial.println("Rotary pulse count: " + String(rotaryPulseCount +1));
+        rotaryPulseCount = 0;
+    }
+    lastRotaryPulse = millis();
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+    pinMode(hookPin, INPUT_PULLUP);
+    pinMode(rotaryPin, INPUT_PULLUP);
+    pinMode(bellPin, OUTPUT);
+    attachInterrupt(digitalPinToInterrupt(rotaryPin), onRotaryPulse, CHANGE);
+    Serial.begin(9600);
+
+    GSM.begin(4800);
+
+    //Serial.println("GET PRODUCT INFO: ");
+    //Serial.println(GSM.getProductInfo());
+//
+    //Serial.println("GET OPERATORS LIST: ");
+    //Serial.println(GSM.getOperatorsList());
+//
+    //Serial.println("GET OPERATOR: ");
+    //Serial.println(GSM.getOperator());
+
 }
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    switch(currentState) {
+        case STATE_IDLE:
+            Serial.println("@Idle");
+            digitalWrite(bellPin, LOW);
+            if (GSM.getCallStatus() == 3) {  //Ringing
+                currentState = STATE_RINGING;
+            }
+            break;
+
+        case STATE_RINGING:
+            Serial.println("@Ringing");
+            ringBell(true);
+            if (isHookOff()) {
+                ringBell(false);
+                GSM.answerCall();
+            }
+            if (GSM.getCallStatus() == 4) {  //Call in progress
+                currentState = STATE_CALLING;
+            }
+
+            break;
+
+        case STATE_CALLING:
+            Serial.println("@Calling");
+            if (digitalRead(hookPin) == HIGH) {
+                GSM.hangoffCall();
+                currentState = STATE_IDLE;
+            }
+            break;
+    }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+
